260a01f53e16019f5ff9e2794478c7051eb96200
sulwan <sulwan@126.com>
2018-02-28 17:29:01 +0800

    增加了属相

--- idcard.go
+++ idcard.go
@@ -40,8 +40,8 @@ func Address(id string) string {
 
 // 获取性别
 func Sex(id string) string {
-	v, _ := strconv.Atoi(id[16:17])
-	if v%2 != 0 {
+	val, _ := strconv.Atoi(id[16:17])
+	if val%2 != 0 {
 		return "男"
 	}
 	return "女"
@@ -49,11 +49,19 @@ func Sex(id string) string {
 
 // 获取生肖
 func Zodiac(id string) string {
-	
+	zodiac := []string{"鼠", "牛", "虎", "兔", "龙", "蛇", "马", "羊", "猴", "鸡", "狗", "猪"}
+	year, _ := strconv.Atoi(Year(id))
+	val := (year - 1900) % 12
+	return zodiac[val]
+}
+
+// 获取年份
+func Year(id string) string {
+	return id[6:10]
 }
 
 func main() {
 	fmt.Println("身份证所在地:" + Address("130105198410040316"))
 	fmt.Println("性别:" + Sex("130105198410040316"))
-	fmt.Println("所属动物:" + Zodiac("130105198410040316"))
+	fmt.Println("所属动物:" + Zodiac("130124198705293638"))
 }
