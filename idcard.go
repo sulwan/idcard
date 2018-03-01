9bd1a32e8d2f961b32a8db426adb928af2c57ffe
sulwan <sulwan@126.com>
2018-03-01 09:24:08 +0800

    增加星座获取

--- idcard.go
+++ idcard.go
@@ -60,8 +60,36 @@ func Year(id string) string {
 	return id[6:10]
 }
 
+// 获取星座
+func constellation(id string) string {
+	constellation := []string{"水瓶座", "双鱼座", "白羊座", "金牛座", "双子座", "巨蟹座", "狮子座", "处女座", "天秤座", "天蝎座", "射手座", "摩羯座"}
+	marginal := []int{20, 19, 21, 20, 21, 22, 23, 23, 23, 24, 23, 22}
+	mon, _ := strconv.Atoi(month(id))
+	month := mon - 1
+	day, _ := strconv.Atoi(day(id))
+	if day < marginal[month] {
+		month--
+	}
+	if month >= 0 {
+		return constellation[month]
+	} else {
+		return constellation[11]
+	}
+}
+
+// 获取月
+func month(id string) string {
+	return id[10:12]
+}
+
+// 获取日
+func day(id string) string {
+	return id[12:14]
+}
+
 func main() {
-	fmt.Println("身份证所在地:" + Address("130105198410040316"))
-	fmt.Println("性别:" + Sex("130105198410040316"))
-	fmt.Println("所属动物:" + Zodiac("130124198705293638"))
+	fmt.Println("身份证所在地:" + Address("130431199402181323"))
+	fmt.Println("性别:" + Sex("130431199402181323"))
+	fmt.Println("所属动物:" + Zodiac("130431199402181323"))
+	fmt.Println("所属星座:" + constellation("130121199409180025"))
 }
